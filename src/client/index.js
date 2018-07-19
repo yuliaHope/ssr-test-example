c586e32252698c9c03feac4358d3dffe3f84b787
Yulia Galuzo <yulia.galuzo9771@gmail.com>
2018-07-20 00:04:53 +0300

    basic set up

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hydrate } from 'react-dom';
+import App from './app';
+
+hydrate(
+  <App />,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path')
+var webpack = require('webpack')
+var nodeExternals = require('webpack-node-externals')
+
+var browserConfig = {
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "true"
+    })
+  ]
+}
+
+var serverConfig = {
+  entry: './src/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: __dirname,
+    filename: 'server.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    })
+  ]
+}
+
+module.exports = [browserConfig, serverConfig]
